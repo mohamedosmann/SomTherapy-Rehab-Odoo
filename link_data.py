8a8bc1e14f25f5db1ff3d48b6784f8a47d0da53d
mohamedosmann <mohamedosmann75@gmail.com>
2026-03-13 21:27:41 +0300

    FIX: Removed all config_parameter records from XML to resolve parsing errors permanently. System will use Python fallbacks.

--- link_data.py
+++ link_data.py
@@ -25,14 +25,15 @@ link_record('account.account', 'account_staff_payable', [('code', '=', '201200')
 link_record('account.account', 'account_cash_on_hand', [('code', '=', '101100')])
 link_record('account.account', 'account_bank_account', [('code', '=', '101400')])
 link_record('account.account', 'account_student_fees', [('code', '=', '401100')])
-link_record('account.account', 'account_teacher_salaries', [('code', '=', '601100')])
-link_record('account.account', 'account_staff_salaries', [('code', '=', '601200')])
+link_record('account.account', 'account_staff_salaries', [('code', '=', '601100')])
 link_record('account.account', 'account_dormitory_expenses', [('code', '=', '601300')])
 link_record('account.account', 'account_supplies', [('code', '=', '601400')])
 
 # Link Journals
 link_record('account.journal', 'journal_cash_payments', [('code', '=', 'CASH')])
 link_record('account.journal', 'journal_bank_payments', [('code', '=', 'BNK')])
+link_record('account.journal', 'journal_evc_payments', [('code', '=', 'EVC')])
+link_record('account.journal', 'journal_salaam_payments', [('code', '=', 'SLM')])
 link_record('account.journal', 'journal_student_invoices', [('code', '=', 'INV')])
 link_record('account.journal', 'journal_staff_expenses', [('code', '=', 'STF')])
 

8e3ecfaf229875c842ac71db70f3008b87e65305
mohamedosmann <mohamedosmann75@gmail.com>
2026-03-15 16:09:24 +0300

    verify version 1.5

--- rehab_management/__manifest__.py
+++ rehab_management/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Rehab Management',
-    'version': '1.4',
+    'version': '1.5',
     'summary': 'Odoo customization for Rehab and Dormitory Management',
     'description': """
         Customization for Rehab and Dormitory Management, 
